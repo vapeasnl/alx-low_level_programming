0ceddb0ea4e6079929209c7a5338161159164468
vapeasnl <samimalim1991@gmail.com>
2023-10-05 13:04:22 -0700

    task 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - program printing line with puts
- * Return : 0 (Success) 
+ * Return : 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\ "Programming is like building a multilingual puzzle");
 return(0);
 }
