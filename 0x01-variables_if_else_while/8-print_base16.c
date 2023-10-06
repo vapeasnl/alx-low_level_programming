0d5b6a36e8e15c7f9bb35f6a4d6630c4a626c252
vapeasnl <samimalim1991@gmail.com>
2023-10-06 11:57:50 -0700

    file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - base 16 in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char l;
+for (i = 0; i <=9; i++)
+{
+putchar(i);
+}
+for (l = 'a'; l <= 'f'; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
