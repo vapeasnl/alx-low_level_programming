f612929ac1a98a7bc10b682b4743aae36125f109
vapeasnl <samimalim1991@gmail.com>
2023-10-06 10:35:51 -0700

    file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - alphabet lowercase, and uppercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+for (x = 'A'; x <= 'Z'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
