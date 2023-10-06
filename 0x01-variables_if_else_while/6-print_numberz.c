cfe58f6ef1a52debd3d480eec831d29ccb4f9bbf
vapeasnl <samimalim1991@gmail.com>
2023-10-06 10:44:22 -0700

    file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - 0 to 10 with putchar and following line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 28; a < 38; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
