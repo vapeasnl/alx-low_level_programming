5c8fd2176f1d56d6fbb2ec8c8c98411f34220521
vapeasnl <samimalim1991@gmail.com>
2023-10-06 10:41:32 -0700

    file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - 0 to 10 by new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
