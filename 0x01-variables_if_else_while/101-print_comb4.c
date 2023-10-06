ce067f0dac42c6144a1a2f32b390dc887163af02
vapeasnl <samimalim1991@gmail.com>
2023-10-06 12:35:34 -0700

    file 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,18 +6,16 @@
 int main(void)
 {
 int a, b, c;
-for (a = 0; a < 9; a++)
+for (a = 0; a <= 9; a++)
 {
-for (b = 0; b < 9; b++)
+for (b = a + 1; b <= 9; b++)
 {
-for (c = 0; c < 9; c++)
+for (c = b + 1; c <= 9; c++)
 {
-if (c > b && b > a)
-{
-putchar(a);
-putchar(b);
-putchar(c);
-if (a != 8 || b != 9)
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+if (!(a == 7 && b == 8 && c == 9))
 {
 putchar(',');
 putchar(' ');
