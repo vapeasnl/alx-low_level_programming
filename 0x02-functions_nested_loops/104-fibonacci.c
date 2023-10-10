305bb0d933f7a21908c2843d626ae431fa031a95
vapeasnl <samimalim1991@gmail.com>
2023-10-10 15:38:36 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 /**
 * main - fibonacci program
-*
-* Purpose - no hardcode
-*
 * Return:  (Success)
 */
 int main(void)
 {
-unsigned long int n;
 unsigned long int avnt = 1;
 unsigned long int aprs = 2;
 unsigned long int l = 10000000000;
-unsigned long int avnt1;
-unsigned long int avnt2;
-unsigned long int aprs1;
-unsigned long int aprs2;
+unsigned long int avnt1 = 0;
+unsigned long int avnt2 = 0;
+unsigned long int aprs1 = 0;
+unsigned long int aprs2 = 0;
 printf("%lu", avnt);
-for (n = 1; n < 91; n++)
+for (int n = 1; n < 91; n++)
 {
 printf(", %lu", aprs);
+unsigned long int temp = aprs;
 aprs += avnt;
-avnt = aprs - avnt;
+avnt = temp;
 }
-avnt1 = (avnt / l);
-avnt1 = (avnt % l);
-aprs1 = (aprs / l);
-aprs2 = (aprs % l);
-for (n = 92; n < 99; ++n)
+for (int n = 92; n < 99; ++n)
 {
 printf(", %lu", aprs1 + (aprs2 / l));
-printf("%lu", aprs2 % l);
-aprs1 = aprs1 + avnt1;
-avnt1 = aprs1 - avnt1;
-aprs2 = aprs2 + avnt2;
-avnt2 = aprs2 - avnt2;
+printf("%010lu", aprs2 % l);
+unsigned long int temp1 = aprs1;
+aprs1 += avnt1;
+avnt1 = temp1;
+unsigned long int temp2 = aprs2;
+aprs2 += avnt2;
+avnt2 = temp2;
 }
+
 printf("\n");
 return (0);
 }
+
