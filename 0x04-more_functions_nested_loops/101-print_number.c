c5b897261545e25797e4e24df4e975a93472d42e
vapeasnl <samimalim1991@gmail.com>
2023-10-12 13:35:55 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_number - integer
+* @n: integer
+*/
+void print_number(int n)
+{
+unsigned int x;
+if (n < 0)
+{
+x = -n;
+_putchar('-');
+}
+else
+{
+x = n;
+}
+if (x / 10)
+{
+print_number(x / 10);
+}
+_putchar((x % 10) + '0');
+}
