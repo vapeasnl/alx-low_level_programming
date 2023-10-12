553a3ef58c1b0baf59fafa4aa3e63896cb1b615b
vapeasnl <samimalim1991@gmail.com>
2023-10-12 13:20:10 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+* main - prints the numbers from 1 to 100
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a % 3 == 0 && a % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (a % 5 == 0 && a % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (a % 3 == 0 && a % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (a == 1)
+{
+printf("%d", a);
+}
+else
+{
+printf(" %d", a);
+}
+}
+printf("\n");
+return (0);
+}
