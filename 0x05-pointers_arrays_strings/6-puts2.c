bd275121ca86abd8005348dbcfbedf44727342b3
vapeasnl <samimalim1991@gmail.com>
2023-10-16 13:14:27 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* puts2 - function should print other character of a string,
+* starting with the first character
+* @str: input
+* Return: print
+*/
+void puts2(char *str)
+{
+int lengh = 0;
+int i = 0;
+char *j = str;
+int k;
+while (*j != '\0')
+{
+j++;
+lengh++;
+}
+i = lengh - 1;
+for (k = 0 ; k <= i ; o++)
+{
+if (k % 2 == 0)
+{
+_putchar(str[k]);
+}
+}
+_putchar('\n');
+}
