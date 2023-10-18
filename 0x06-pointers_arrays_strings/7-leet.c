0c78597593e8f53d76f8baf3764d8fd117b4092f
vapeasnl <samimalim1991@gmail.com>
2023-10-18 14:03:23 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - a function that encodes a string into 1337
+ * @str: input value
+ * Return: str value
+ */
+char *leet(char *str)
+{
+	int a, b;
+	char ch1[] = "aAeEoOtTlL";
+	char ch2[] = "4433007711";
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		for (b = 0; b< 10; b++)
+		{
+			if (str[a] == ch1[b])
+			{
+				str[a] = ch2[b];
+			}
+		}
+	}
+	return (str);
+}
+
