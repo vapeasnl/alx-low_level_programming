85563c9eb6d386115dbce65ee823e741793696ea
vapeasnl <samimalim1991@gmail.com>
2023-10-25 13:21:43 -0700

    TASK 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _strlen_recurse - returns the length
+* _strlength_recurse - returns the length
 * @s: string to calculate the length
 * Return: length of the string
 */
-int _strlen_recurse(char *s)
+int _strlength_recurse(char *s)
 {
 if (*s == '\0')
 return (0);
@@ -23,7 +23,7 @@ if (*(s + x) != *(s + length - 1))
 return (0);
 if (x >= length)
 return (1);
-return (check_pal(s, x + 1, length - 1));
+return (check_palindrome(s, x + 1, length - 1));
 }
 /**
 * is_palindrome - a function that returns 1 if a string
@@ -37,3 +37,5 @@ if (*s == 0)
 return (1);
 return (check_palindrome(s, 0, _strlength_recurse(s)));
 }
+
+
