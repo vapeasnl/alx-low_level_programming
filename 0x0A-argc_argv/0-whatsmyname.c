14b8e582176f787f8638f66e373ff56a502fed17
vapeasnl <samimalim1991@gmail.com>
2023-10-27 12:14:01 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>		
+#include "main.h"
+/**
+* main - a program that prints the number
+* of arguments passed into it
+* @argc: number of arg
+* @argv: array of arg
+* Return: Always 0 (Success)
+*/
+int main(int argc _attribute_((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
