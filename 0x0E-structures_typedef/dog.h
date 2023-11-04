8cc8825b161317b6042e5ee5d85ab70fdd05c6a4
vapeasnl <samimalim1991@gmail.com>
2023-11-04 14:55:50 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,8 @@ char *owner;
 typedef struct dog dog_inf;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+dog_inf *new_dog(char *name, float age, char *owner);
+void free_dog(dog_inf *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 #endif
