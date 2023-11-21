010493eed97ad4b19b4fc2150dc071ca35f45f87
SalimAbdessamad <samimalim1991@gmail.com>
2023-11-21 11:44:32 -0800

    betty update task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,7 @@ size_t print_listint_safe(const listint_t *head);
 /**
 * looped_len - Counts the number of unique nodes
 * @head: A pointer to the head of the listint_t to check.
-* Return: If the list is not looped - 0 
+* Return: If the list is not looped - 0
 * or the number of unique nodes in the list.
 */
 size_t looped_len(const listint_t *head)
