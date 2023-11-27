abb6f0443a5f7e20fa4ad8465fc0b8bcf82cc49b
SalimAbdessamad <samimalim1991@gmail.com>
2023-11-27 13:19:37 -0800

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -24,12 +24,7 @@ void elf_check(unsigned char *e_ident)
 int index;
 for (index = 0; index < 4; index++)
 {
-if (e_ident[index] != 127 && e_ident[index] != 'E')
-{
-dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-exit(98);
-}
-if (e_ident[index] != 'L' && e_ident[index] != 'F')
+if (e_ident[index] != 127 && e_ident[index] != 'E' && e_ident[index] != 'L' && e_ident[index] != 'F')
 {
 dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 exit(98);
