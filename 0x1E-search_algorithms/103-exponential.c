19008b4eef199dff3a9dacbbc21f9003946eecbe
abdessamadsalim <samimalim1991@gmail.com>
2024-05-07 09:35:43 -0700

    task 103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -28,3 +28,24 @@ left = x + 1;
 }
 return (-1);
 }
+/**
+* exponential_search - Searches for a value in a sorted array
+* @array: A pointer to the first element
+* @size: The number of elements
+* @value: The value
+* Return: If the value is not present or the array is NULL, -1.
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+size_t i = 0, r;
+if (array == NULL)
+return (-1);
+if (array[0] != value)
+{
+for (i = 1; i < size && array[i] <= value; i = i * 2)
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+}
+r = i < size ? i : size - 1;
+printf("Value found between indexes [%ld] and [%ld]\n", i / 2, r);
+return (_binary_search(array, i / 2, r, value));
+}
